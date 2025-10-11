8112c1a42194d3116525ddf861573665ed9bbc9b
Ben Macaro <b.macaro0325@gmail.com>
2025-10-10 21:37:01 -0400

    vmem

--- kernel.h
+++ kernel.h
@@ -7,6 +7,13 @@
 #define PROC_UNUSED     0
 #define PROC_RUNNABLE   1
 
+#define SATP_SV32   (1u << 31)
+#define PAGE_V      (1 << 0)
+#define PAGE_R      (1 << 1)
+#define PAGE_W      (1 << 2)
+#define PAGE_X      (1 << 3)
+#define PAGE_U      (1 << 4)
+
 #define PANIC(fmt, ...)                                                         \
     do {                                                                        \
         printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);   \
@@ -17,6 +24,7 @@ struct process {
     int pid;
     int state;
     vaddr_t sp;
+    uint32_t *page_table;
     uint8_t stack[8192];
 };
 
